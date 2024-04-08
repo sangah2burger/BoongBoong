1fad53e1f56a955f1fd29b94a7262fb71c0c5b78
상아이버거 <sangah2burger@sang-aibeogeoui-MacBookPro.local>
2024-04-08 18:13:22 +0900

    project setting

--- BoongBoong/ViewController.swift
+++ BoongBoong/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import TMapSDK
+import CoreLocation
+import Kingfisher
+import Alamofire
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController,TMapViewDelegate {
+    var mapView : TMapView?
+    
+    @IBOutlet weak var mapContainerView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.mapView = TMapView(frame: mapContainerView.frame)
+        self.mapView?.delegate = self
+        guard let apiKey = Bundle.main.apiKey else {
+            print("API키를 로드하지 못했습니다.")
+            return
+        }
+        self.mapView?.setApiKey(apiKey)
+        mapContainerView.addSubview(mapView!)
+        print("성공..")
     }
 
 
 }
 
+extension Bundle {
+    var apiKey : String? {
+        return infoDictionary?["API_KEY"] as? String
+    }
+}
